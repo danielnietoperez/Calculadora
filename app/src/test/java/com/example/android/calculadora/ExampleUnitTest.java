63aa0ab96c021bab1031da7e5865c244e128f758
Daniel <danielnietoperez02@gmail.com>
2018-04-07 15:11:30 -0500

    Calculadora funcionando terminado

--- app/src/test/java/com/example/android/calculadora/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/calculadora/ExampleUnitTest.java
@@ -11,7 +11,20 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros() {
+        double a = 4, b = 6;
+        assertEquals(10,Metodos.Sumar(a,b),0);
+    }
+    public void restar_dos_numeros() {
+        double a = 4, b = 6;
+        assertEquals(-2,Metodos.Sumar(a,b),0);
+    }
+    public void multiplicar_dos_numeros() {
+        double a = 4, b = 6;
+        assertEquals(24,Metodos.Sumar(a,b),0);
+    }
+    public void dividir_dos_numeros() {
+        double a = 5, b = 5;
+        assertEquals(1,Metodos.Sumar(a,b),0);
     }
 }
